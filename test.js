0ead03e1d7d021e4fcc529a56b5ce149817b0d5b
Dziugas <dziugas@graphity.org>
2017-04-01 19:51:57 +0300

    pirmas

--- test.js
+++ test.js
@@ -2,6 +2,21 @@
  * Created by User on 2017-03-29.
  */
 //the first alert
-alert("Hello, World!");
+//alert("Hello, World!");
 //the second alert
-alert("Hello Again!");
+//alert("Hello Again!");
+
+/*    if - else if
+var myAge = 39;
+if (myAge > 30){
+    document.writeln("you're over 30");
+}
+else if (myAge > 20){
+    document.writeln("you're over 20");
+}
+else if(myAge > 10){
+    document.writeln("you're over 10");
+}
+else {
+    document.writeln("fuk off, you're under 100");
+}*/
