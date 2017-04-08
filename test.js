1d9730984039fd3fe8875c35ed96982f889304a3
Dziugas <dziugas@graphity.org>
2017-04-08 12:01:56 +0300

    su pavyzdziais

--- test.js
+++ test.js
@@ -7,6 +7,7 @@
 //alert("Hello Again!");
 
 /*    if - else if
+
 var myAge = 39;
 if (myAge > 30){
     document.writeln("you're over 30");
@@ -20,3 +21,25 @@ else if(myAge > 10){
 else {
     document.writeln("fuk off, you're under 100");
 }*/
+
+/* Logical Operators
+
+var myAge = 28;
+if (myAge < 18 || myAge > 65 || myAge ===48) {
+    document.writeln("ne ne ne, tu arba pensas, arba nepilnametis!");
+}
+else {
+
+    document.writeln("Tu pilnametis, bet dar ne pensininkas!");
+}
+*/
+
+
+/*
+var myAge = 3;
+while(myAge<11){
+    console.log("we're less than 11");
+    myAge++;
+}
+document.writeln("we're equal or over 11");
+*/
