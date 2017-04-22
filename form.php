b911a5ee9af82358c87b9415233817fa7362ab02
Dziugas <dziugas@graphity.org>
2017-04-22 22:14:43 +0300

    forma, php, iframe

---
+++ form.php
@@ -0,0 +1,13 @@
+<html>
+<body>
+
+    Tavo vardas yra <?php echo $_POST["name"]; ?> <br>
+    Tavo pavardė yra <?php echo $_POST["surname"]; ?> <br>
+    Tavo el. pašto adresas yra <?php echo $_POST["email"]; ?> <br>
+    Tavo idėja: <?php echo $_POST["text"]; ?> <br>
+    Tavo nuoroda: <?php echo $_POST["link"]; ?>
+
+
+
+</body>
+</html>
