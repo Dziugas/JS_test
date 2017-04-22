ac1488c78a1e4a2a9eb3edb61b5952ec214888b7
Dziugas <dziugas@graphity.org>
2017-04-22 23:18:01 +0300

    php form

--- form.php
+++ form.php
@@ -11,3 +11,35 @@
 
 </body>
 </html>
+
+<br>
+<br>
+
+
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "forma";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO forma (name, surname, email, text, link)
+VALUES ('name', 'surname', 'email', 'text', 'link')";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+?>
